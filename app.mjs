005b1b0ddebc176d1c40b0a349ab981b8611962c
cruise <abdulla16161616@gmail.com>
2024-10-31 20:36:23 +0300

    cleaned app.mjs code

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import path from 'path'
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 //import Article from './models/Article.mjs';
 //import mongoose from 'mongoose'
 const app = express();
@@ -10,22 +10,11 @@ const port = 4242;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-/*mongoose
-	.connect(
-		"mongodb+srv://malmadar36:musleh36@myfirstdatabase.8efurkp.mongodb.net/?retryWrites=true&w=majority"
-	)
-	.then(() => {
-		console.log("connected successfully");
-	})
-	.catch((error) => {
-		console.log("error with connecting with the DB ", error);
-	});*/
-
-app.get('/calculator', async (req, res) => {
-    const indexPath = path.join(__dirname, 'views', 'calculator.html');
-    res.sendFile(indexPath);
+app.get("/calculator", async (req, res) => {
+  const indexPath = path.join(__dirname, "views", "calculator.html");
+  res.sendFile(indexPath);
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
